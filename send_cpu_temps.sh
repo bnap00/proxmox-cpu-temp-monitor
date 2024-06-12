ddc17e4ef1b2232ae7ee7bbdf2bcb2e560dc5797
Bharat Parsiya <bnap00@gmail.com>
2024-06-12 11:34:22 +0530

    fix core temps

--- send_cpu_temps.sh
+++ send_cpu_temps.sh
@@ -12,14 +12,13 @@ cpu_temp_data=$(echo "$sensor_data" | jq -r "
   to_entries |
   map(
     if .key == \"Package id 0\" and .value.temp1_input != null then
-      \"$MEASUREMENT_NAME,core=Package_id_0 temp1_input=\\(.value.temp1_input)\"
+      \"$MEASUREMENT_NAME,core=Package_id_0 package_temp=\(.value.temp1_input)\"
     elif .key | test(\"Core [0-9]+\") then
+      .key as \$core |
       .value | to_entries | map(
-        if .key | test(\"temp[0-9]+_input\") and .value != null then
-          \"$MEASUREMENT_NAME,core=\\(.key | gsub(\" \"; \"_\")) temp_input=\\(.value)\"
-        else
-          empty
-        end
+        select(.key | test(\"temp[0-9]+_input\")) |
+        select(.value != null) |
+        \"$MEASUREMENT_NAME,core=Core_\((\$core | gsub(\"Core \"; \"\") | tonumber)) core_temp=\(.value)\"
       ) | join(\"\n\")
     else
       empty
